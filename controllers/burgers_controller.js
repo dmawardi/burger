49cfc9c4bf9387f7c80aa11c17088a24d9d4e414
Dannar Mawardi <dmawardi@gmail.com>
2019-09-14 21:44:37 +1000

    Front end functional

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var burger = require('../models/burger');
+var exphbs = require('express-handlebars');
+
+// Home Page route
+router.get('/', function(req, res){
+    burger.select().then(function(response){
+        console.log(response);
+        res.render('index', {burgers: response})
+    });
+});
+
+// Create new burger
+router.post('/api/burgers', function(req, res){
+    let newBurger = req.body.burger_name;
+    burger.create(newBurger);
+});
+
+// Update burger as devoured
+router.put('/api/burgers/:id', function(req, res){
+    var idToDevour = req.params.id;
+    burger.devour(idToDevour);
+});
+
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,14 +1,14 @@
 var orm = require('../config/orm');
 
-function filterDevoured(data) {
-    let filtered = [];
-    for (let i = 0; i < data.length; i++) {
-        if (!data[i].devoured) {
-            filtered.push(data[i]);
-        }
-    }
-    return filtered;
-}
+// function filterDevoured(data) {
+//     let filtered = [];
+//     for (let i = 0; i < data.length; i++) {
+//         if (!data[i].devoured) {
+//             filtered.push(data[i]);
+//         }
+//     }
+//     return filtered;
+// }
 
 burger = {
     create: function (burger_name) {
@@ -32,8 +32,6 @@ burger = {
     select: function () {
         return new Promise(function (resolve, reject) {
         orm.selectAll('burgers').then(function(res){
-            // Filter out the devoured burgers to show only undevoured
-            res = filterDevoured(res);
             resolve(res);
         }).catch(function(err){
             reject(err);
