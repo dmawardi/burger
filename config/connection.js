4dba7d342d2ddaec5d49a10998fcacb9cbb7d175
Dannar <dmawardi@gmail.com>
2019-09-16 21:02:02 +1000

    Connection to Jaws DB built into connection file

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,21 @@ var sql = require('mysql');
 // Password reference ###
 var pass = require('./pw');
 
+// If JAWS DB detected, connecto to it
+if (process.env.JAWSDB_URL) {
+  connection = sql.createConnection(process.env.JAWSDB_URL);
 
-// Build connection to local SQL database
-var connection = sql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : pass,
-    database : 'burgers_db'
-  });
+  // Else, Build connection to local SQL database
+} else {
+  var connection = sql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : pass,
+      database : 'burgers_db'
+    });
+
+}
+
 
 // Export
 module.exports = connection;
